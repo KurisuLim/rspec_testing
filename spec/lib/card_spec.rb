9d2c0246752974ad8512e896e92143a2f1a9d8f9
KurisuLim <kazuki1002003@yahoo.com>
2018-10-12 16:06:13 -0700

    adds shared examples for and it behaves like

--- spec/lib/card_spec.rb
+++ spec/lib/card_spec.rb
@@ -38,11 +38,7 @@ context 'equality' do
             end
         end
 
-    describe 'comparing to a card of different suit' do
-        def other
-            @other   ||= card(suit: :hearts, rank: 4)
-        end
-
+    shared_examples_for 'an unequal card' do
         it 'is not equal' do
             raise unless subject != other
         end
@@ -51,19 +47,20 @@ context 'equality' do
             raise unless Set.new([subject,other]).size == 2
         end
     end
+    describe 'comparing to a card of different suit' do
+        def other
+            @other   ||= card(suit: :hearts, rank: 4)
+        end
+
+        it_behaves_like 'an unequal card'
+    end
 
     describe 'comparing to a card of different rank' do
         def other
             @other   ||= card(suit: :spades, rank: 5)
         end
 
-        it 'is not equal' do
-            raise unless subject != other
-        end
-
-        it 'is hash not equal' do
-            raise unless Set.new([subject,other]).size == 2
-        end
+        it_behaves_like 'an unequal card'
     end
 end
     describe 'a jack' do
